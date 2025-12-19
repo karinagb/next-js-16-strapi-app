5909e5393d6026c328ef78d91c30d8f23e69f7cc
karinagb <karin.gb27@gmail.com>
2025-12-18 18:19:03 -0600

    Remove hardcoded routes

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function AuthLayout({ 
-    children }: { 
-        readonly children: React.ReactNode }) {
-  return (
-      <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-        {children}
-        </div>
-  );    
-}
